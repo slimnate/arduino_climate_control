d6ea5ac53cdcd4dd997fa150b557569110a761be
Slim Nate <slimnatekc@gmail.com>
2021-04-07 17:43:05 -0500

    implement basic scheduling library for light controller

---
+++ Scheduling.cpp
@@ -0,0 +1,49 @@
+#include "Scheduling.h"
+
+// ScheduleEntry class
+ScheduleEntry::ScheduleEntry(Time dayStart, Time nightStart)
+    : dayStart(dayStart), nightStart(nightStart) { };
+
+DayNight ScheduleEntry::getDayNight(Time t) {
+    // if time lt dayStart OR gte nightStart, night
+    if(t.compare(dayStart) < 0 || t.compare(nightStart) >= 0) {
+        return NIGHT;
+    }
+
+    // if time gte dayStart AND lt nightStart, day
+    if(t.compare(dayStart) >= 0 && t.compare(nightStart) < 0) {
+        return DAY;
+    }
+
+    // default to DAY if for some reason neither test matches.
+    // Also log error, since this should not happen.
+    Serial.println("ERROR: ScheduleEntry.getDayNight did not match either day or night. Defaulting to DAY.");
+    Serial.print("Time: ");
+    Serial.print(t.hours); Serial.print(":");
+    Serial.print(t.minutes); Serial.print(":");
+    Serial.println(t.seconds);
+
+    return DAY;
+};
+
+// Fixed schedule implementation
+FixedSchedule::FixedSchedule(ScheduleEntry* entry) {
+    this->entry = entry;
+};
+
+ScheduleEntry* FixedSchedule::getEntry(Date d) {
+    //ignores the date passed and always returns the same entry
+    return entry;
+}
+
+
+//MonthlySchedule implementation
+MonthlySchedule::MonthlySchedule(ScheduleEntry* sched[12]) {
+    for(int i=0; i<12; i++) {
+        schedules[i] = sched[i];
+    }
+};
+
+ScheduleEntry* MonthlySchedule::getEntry(Date d) {
+    return schedules[d.month-1];
+}
---
+++ Scheduling.h
@@ -0,0 +1,35 @@
+#include "DateTime.h"
+
+enum DayNight { DAY, NIGHT };
+
+//abstract Schedule class
+struct Schedule {
+    virtual ScheduleEntry* getEntry(Date) = 0;
+};
+
+//schedule entry class
+struct ScheduleEntry {
+    Time dayStart;
+    Time nightStart;
+
+    ScheduleEntry(Time, Time);
+    DayNight getDayNight(Time);
+};
+
+//fixed schedule implementation
+struct FixedSchedule : public Schedule {
+    ScheduleEntry* entry;
+
+    FixedSchedule(ScheduleEntry*);
+    ScheduleEntry* getEntry(Date);
+};
+
+//monthly schedule implementation
+struct MonthlySchedule : public Schedule {
+    ScheduleEntry* schedules[12];
+
+    MonthlySchedule(ScheduleEntry*[12]);
+    ScheduleEntry* getEntry(Date);
+};
+
+//TODO: other schedule implementations
